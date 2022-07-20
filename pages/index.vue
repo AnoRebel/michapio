560c3e8f5379f29428ede1b9364d7012a3da71b1
AnoRebel <hacker4rebel@gmail.com>
2022-07-20 03:27:16 +0300

    Changelog:
    - Fixed sidebar layout

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,9 @@
       <ChapioCard :data="{}" />
       <template #aside>
         <div class="h-full w-full flex flex-col">
-          <div class="h-1/2 w-full flex">
+          <div class="h-1/2 w-full flex flex-col">
             <div class="w-full text-underline">Recent Michapio</div>
-            <div class="w-full flex flex-col">
+            <div class="w-full flex">
               <ul class="w-full">
                 <li class="w-full">
                   <div class="w-full rounded-md shadow-md h-20 m-2 bg-slate-500">1</div>
@@ -17,7 +17,9 @@
               </ul>
             </div>
           </div>
-          <div class="h-1/2 w-full flex"><div class="text-underline">Most Viewed</div></div>
+          <div class="h-1/2 w-full flex flex-col">
+            <div class="text-underline">Most Viewed</div>
+          </div>
         </div>
       </template>
     </NuxtLayout>
