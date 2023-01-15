152bfc3cf5f7979dcd3cecf7f8110294a2c16f11
AnoRebel <hacker4rebel@gmail.com>
2023-01-16 01:58:43 +0300

    Changelog: - Still working on auth tabs ui

--- components/Profile.vue
+++ components/Profile.vue
@@ -29,6 +29,9 @@ const tabs = [
   { name: "Likes", href: "#", current: true, show: isLoggedIn() },
   { name: "Favourites", href: "#", current: false, show: isLoggedIn() },
 ];
+const tabClass = computed(() =>
+  tabs.filter(tabC => tabC.show == true).length == 1 ? "w-full" : `w-1/${tabs.length}`
+);
 const team = [
   {
     name: "Leslie Alexander",
@@ -139,7 +142,7 @@ const team = [
                                 tab.current
                                   ? 'border-indigo-500 text-indigo-600'
                                   : 'border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700',
-                                `w-1/${tabs.length} whitespace-nowrap border-b-2 px-1 pb-4 text-center text-sm font-medium`,
+                                `whitespace-nowrap border-b-2 px-1 pb-4 text-center text-sm font-medium ${tabClass}`,
                               ]"
                             >
                               {{ tab.name }}
